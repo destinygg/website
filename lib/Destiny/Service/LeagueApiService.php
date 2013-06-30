fa07c242d198e3b76b935498890cd7fbd1086953
Cene <cene@cene.co.za>
2013-06-30 18:52:56 +0200

    Removed some old config, the league api /ingame/ call now passes the last game it retrieved, the api will return no data if the games are the same

--- lib/Destiny/Service/LeagueApiService.php
+++ lib/Destiny/Service/LeagueApiService.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Destiny\Service;
 
 use Destiny\Service;
@@ -32,77 +31,78 @@ class LeagueApiService extends Service {
 
 	public function getStatus(array $options = array()) {
 		return new CurlBrowser ( array_merge ( array (
-				'url' => Config::$a ['lolapi'] ['url'],
-				'contentType' => MimeType::JSON,
-				'onfetch' => function ($json) {
-					if (false == $json ['success'] && $json ['data'] != null) {
-						throw new AppException ( 'LoL API down.' );
-					}
-					return $json ['data'];
-				} 
+			'url' => Config::$a ['lolapi'] ['url'],
+			'contentType' => MimeType::JSON,
+			'onfetch' => function ($json) {
+				if (false == $json ['success'] && $json ['data'] != null) {
+					throw new AppException ( 'LoL API down.' );
+				}
+				return $json ['data'];
+			} 
 		), $options ) );
 	}
 
 	public function getLeague(array $summoner) {
 		$playerLeague = new CurlBrowser ( array (
-				'url' => new String ( Config::$a ['lolapi'] ['url'] . '{summoner.region}/{summoner.name}/league?key={apikey}', array (
-						'summoner.region' => $summoner ['region'],
-						'summoner.name' => utf8_decode ( $summoner ['name'] ),
-						'apikey' => Config::$a ['lolapi'] ['apikey'] 
-				) ),
-				'contentType' => MimeType::JSON,
-				'params' => $summoner,
-				'onfetch' => function ($league, $summoner) {
-					if (false == $league ['success']) {
-						throw new AppException ( 'LoL API down.' );
-					}
-					$league ['data'] ['rankInt'] = Lol::rankToInt ( $league ['data'] ['rank'] );
-					return $league ['data'];
-				} 
+			'url' => new String ( Config::$a ['lolapi'] ['url'] . '{summoner.region}/{summoner.name}/league?key={apikey}', array (
+				'summoner.region' => $summoner ['region'],
+				'summoner.name' => utf8_decode ( $summoner ['name'] ),
+				'apikey' => Config::$a ['lolapi'] ['apikey'] 
+			) ),
+			'contentType' => MimeType::JSON,
+			'params' => $summoner,
+			'onfetch' => function ($league, $summoner) {
+				if (false == $league ['success']) {
+					throw new AppException ( 'LoL API down.' );
+				}
+				$league ['data'] ['rankInt'] = Lol::rankToInt ( $league ['data'] ['rank'] );
+				return $league ['data'];
+			} 
 		) );
 		return $playerLeague->getResponse ();
 	}
 
 	public function getRecentGames(array $summoner, $limit = 10) {
 		$games = new CurlBrowser ( array (
-				'url' => new String ( Config::$a ['lolapi'] ['url'] . '{summoner.region}/{summoner.name}/games?key={apikey}&limit={limit}', array (
-						'summoner.region' => $summoner ['region'],
-						'summoner.name' => utf8_decode ( $summoner ['name'] ),
-						'apikey' => Config::$a ['lolapi'] ['apikey'],
-						'limit' => $limit 
-				) ),
-				'contentType' => MimeType::JSON 
+			'url' => new String ( Config::$a ['lolapi'] ['url'] . '{summoner.region}/{summoner.name}/games?key={apikey}&limit={limit}', array (
+				'summoner.region' => $summoner ['region'],
+				'summoner.name' => utf8_decode ( $summoner ['name'] ),
+				'apikey' => Config::$a ['lolapi'] ['apikey'],
+				'limit' => $limit 
+			) ),
+			'contentType' => MimeType::JSON 
 		) );
 		return $games->getResponse ();
 	}
 
-	public function getInGameProgress(array $summoner) {
+	public function getInGameProgress(array $summoner, $fileGameId = null) {
 		$progress = new CurlBrowser ( array (
-				'url' => new String ( Config::$a ['lolapi'] ['url'] . '{summoner.region}/{summoner.name}/ingame?key={apikey}', array (
-						'summoner.region' => $summoner ['region'],
-						'summoner.name' => utf8_decode ( $summoner ['internalName'] ),
-						'apikey' => Config::$a ['lolapi'] ['apikey'] 
-				) ),
-				'contentType' => MimeType::JSON 
+			'url' => new String ( Config::$a ['lolapi'] ['url'] . '{summoner.region}/{summoner.name}/ingame?key={apikey}&gameId={gameId}', array (
+				'summoner.region' => $summoner ['region'],
+				'summoner.name' => utf8_decode ( $summoner ['internalName'] ),
+				'gameId' => $fileGameId,
+				'apikey' => Config::$a ['lolapi'] ['apikey'] 
+			) ),
+			'contentType' => MimeType::JSON 
 		) );
 		return $progress->getResponse ();
 	}
 
 	public function getSummoner(array $summoner) {
 		$playerSummoner = new CurlBrowser ( array (
-				'url' => new String ( Config::$a ['lolapi'] ['url'] . '{summoner.region}/{summoner.name}?key={apikey}', array (
-						'summoner.region' => $summoner ['region'],
-						'summoner.name' => utf8_decode ( $summoner ['name'] ),
-						'apikey' => Config::$a ['lolapi'] ['apikey'] 
-				) ),
-				'contentType' => MimeType::JSON,
-				'onfetch' => function ($json) {
-					if (false == $json ['success']) {
-						throw new AppException ( 'LoL API down.' );
-					}
-					$json ['data'] ['revisionDate'] = Date::getDateTime ( floatval ( $json ['data'] ['revisionDate'] ) / 1000 )->format ( Date::FORMAT );
-					return $json;
-				} 
+			'url' => new String ( Config::$a ['lolapi'] ['url'] . '{summoner.region}/{summoner.name}?key={apikey}', array (
+				'summoner.region' => $summoner ['region'],
+				'summoner.name' => utf8_decode ( $summoner ['name'] ),
+				'apikey' => Config::$a ['lolapi'] ['apikey'] 
+			) ),
+			'contentType' => MimeType::JSON,
+			'onfetch' => function ($json) {
+				if (false == $json ['success']) {
+					throw new AppException ( 'LoL API down.' );
+				}
+				$json ['data'] ['revisionDate'] = Date::getDateTime ( floatval ( $json ['data'] ['revisionDate'] ) / 1000 )->format ( Date::FORMAT );
+				return $json;
+			} 
 		) );
 		$data = $playerSummoner->getResponse ();
 		if ($data == null || $data ['success'] == false) {
@@ -119,8 +119,7 @@ class LeagueApiService extends Service {
 		$lookupSummoners = Config::$a ['lol'] ['summoners'];
 		$summoners = array ();
 		foreach ( $lookupSummoners as $info ) {
-			if (! $info ['public'])
-				continue;
+			if (! $info ['public']) continue;
 			$summoner = $this->getSummoner ( $info );
 			$summoner ['region'] = Lol::getRegion ( $info ['region'] );
 			$summoner ['league'] = ($info ['stats']) ? $this->getLeague ( $info ) : null;
