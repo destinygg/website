5ecacfe26fb03e38135b28180331d79b9cfd25a8
Cene <cene@cene.co.za>
2013-07-04 01:53:40 +0200

    Fixed payment cancel, when paypal IPN fails

--- lib/Destiny/Action/Payment/Cancel.php
+++ lib/Destiny/Action/Payment/Cancel.php
@@ -1,7 +1,8 @@
 <?php
-
 namespace Destiny\Action\Payment;
 
+use PayPal\PayPalAPI\GetRecurringPaymentsProfileDetailsReq;
+use PayPal\PayPalAPI\GetRecurringPaymentsProfileDetailsRequestType;
 use Destiny\Application;
 use Destiny\Service\SubscriptionsService;
 use Destiny\Service\OrdersService;
@@ -35,7 +36,7 @@ class Cancel {
 		}
 		// We always need an active profile
 		if (empty ( $paymentProfile ) || strcasecmp ( $paymentProfile ['state'], 'ActiveProfile' ) !== 0) {
-			throw new AppException ( sprintf ( 'Payment profile is not active' ) );
+			throw new AppException ( sprintf ( 'Payment profile is not active %s', $paymentProfile ['state'] ) );
 		}
 		
 		$model->title = 'Cancel scheduled payment';
@@ -56,26 +57,45 @@ class Cancel {
 			// Reset confirmation
 			Session::set ( 'confirmationId' );
 			
-			// Do we have a payment profile, we need to cancel it with paypal
-			$manageRPPStatusReqestDetails = new ManageRecurringPaymentsProfileStatusRequestDetailsType ();
-			$manageRPPStatusReqestDetails->Action = 'Cancel';
-			$manageRPPStatusReqestDetails->ProfileID = $paymentProfile ['paymentProfileId'];
-			
-			$manageRPPStatusReqest = new ManageRecurringPaymentsProfileStatusRequestType ();
-			$manageRPPStatusReqest->ManageRecurringPaymentsProfileStatusRequestDetails = $manageRPPStatusReqestDetails;
-			
-			$manageRPPStatusReq = new ManageRecurringPaymentsProfileStatusReq ();
-			$manageRPPStatusReq->ManageRecurringPaymentsProfileStatusRequest = $manageRPPStatusReqest;
-			
+			// PPService
 			$paypalService = new PayPalAPIInterfaceServiceService ();
-			$manageRPPStatusResponse = $paypalService->ManageRecurringPaymentsProfileStatus ( $manageRPPStatusReq );
 			
-			if (isset ( $manageRPPStatusResponse ) && $manageRPPStatusResponse->Ack == 'Success') {
-				$paymentProfile ['state'] = 'Cancelled';
-				$orderService->updatePaymentProfileState ( $paymentProfile ['profileId'], $paymentProfile ['state'] );
-			} else {
-				throw new AppException ( $manageRPPStatusResponse->Errors [0]->LongMessage );
+			$getRPPDetailsReqest = new GetRecurringPaymentsProfileDetailsRequestType ();
+			$getRPPDetailsReqest->ProfileID = $paymentProfile ['paymentProfileId'];
+			$getRPPDetailsReq = new GetRecurringPaymentsProfileDetailsReq ();
+			$getRPPDetailsReq->GetRecurringPaymentsProfileDetailsRequest = $getRPPDetailsReqest;
+			$getRPPDetailsResponse = $paypalService->GetRecurringPaymentsProfileDetails ( $getRPPDetailsReq );
+			if (empty ( $getRPPDetailsResponse ) || $getRPPDetailsResponse->Ack != 'Success') {
+				throw new AppException ( 'Error retrieving payment profile status' );
 			}
+			$profileStatus = $getRPPDetailsResponse->GetRecurringPaymentsProfileDetailsResponseDetails->ProfileStatus;
+			// Active profile, send off the cancel
+			if (strcasecmp ( $profileStatus, 'ActiveProfile' ) === 0 || strcasecmp ( $profileStatus, 'CancelledProfile' ) === 0) {
+				if (strcasecmp ( $profileStatus, 'ActiveProfile' ) === 0) {
+					// Do we have a payment profile, we need to cancel it with paypal
+					$manageRPPStatusReqestDetails = new ManageRecurringPaymentsProfileStatusRequestDetailsType ();
+					$manageRPPStatusReqestDetails->Action = 'Cancel';
+					$manageRPPStatusReqestDetails->ProfileID = $paymentProfile ['paymentProfileId'];
+					
+					$manageRPPStatusReqest = new ManageRecurringPaymentsProfileStatusRequestType ();
+					$manageRPPStatusReqest->ManageRecurringPaymentsProfileStatusRequestDetails = $manageRPPStatusReqestDetails;
+					
+					$manageRPPStatusReq = new ManageRecurringPaymentsProfileStatusReq ();
+					$manageRPPStatusReq->ManageRecurringPaymentsProfileStatusRequest = $manageRPPStatusReqest;
+					
+					$manageRPPStatusResponse = $paypalService->ManageRecurringPaymentsProfileStatus ( $manageRPPStatusReq );
+					if (! isset ( $manageRPPStatusResponse ) || $manageRPPStatusResponse->Ack != 'Success') {
+						throw new AppException ( $manageRPPStatusResponse->Errors [0]->LongMessage );
+					}
+				} else {
+					$log = Application::instance ()->getLogger ();
+					$log->info ( sprintf ( 'Payment profile cancelled from status [%s]', $profileStatus ) );
+				}
+				$orderService->updatePaymentProfileState ( $paymentProfile ['profileId'], 'Cancelled' );
+			} else {
+				throw new AppException ( sprintf ( 'Invalid payment profile status [%s] %s', $profileStatus, $paymentProfile ['paymentProfileId'] ) );
+			}
+			
 			// Cancel subscription status
 			if (! empty ( $subscription )) {
 				$subService->updateSubscriptionRecurring ( $subscription ['subscriptionId'], false );
@@ -84,7 +104,9 @@ class Cancel {
 			// Show unsubscribed screen
 			$model->unsubscribed = true;
 			return 'paymentcancel';
-		} else {
+		} else 
+
+		{
 			throw new AppException ( 'Invalid confirmation id' );
 		}
 		throw new AppException ( 'Could not cancel subscription' );
