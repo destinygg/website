ff4665525bc2e29396bb18f7fd92277ba622ae6f
Cene <cene@cene.co.za>
2013-07-06 02:51:20 +0200

    Fixed order of icons

--- static/chat/js/gui.js
+++ static/chat/js/gui.js
@@ -526,25 +526,16 @@ function ChatUser(args){
 };
 ChatUser.prototype.getFeatureHTML = function(){
 	var icons = '';
-	for (var i = this.features.length - 1; i >= 0; i--) {
-		switch(this.features[i]){
-			case UserFeatures.SUBSCRIBER :
-				icons += '<i class="icon-subscriber" title="Subscriber"/>';
-				break;
-			case UserFeatures.VIP :
-				icons += '<i class="icon-vip" title="VIP"/>';
-				break;
-			case UserFeatures.MODERATOR :
-				icons += '<i class="icon-moderator" title="Moderator"/>';
-				break;
-			case UserFeatures.ADMIN :
-				icons += '<i class="icon-administrator" title="Administrator"/>';
-				break;
-			case UserFeatures.BOT :
-				icons += '<i class="icon-bot" title="Bot"/>';
-				break;
-		}
-	}
+	if($.inArray(UserFeatures.BOT, this.features))
+		icons += '<i class="icon-bot" title="Bot"/>';
+	if($.inArray(UserFeatures.ADMIN, this.features))
+		icons += '<i class="icon-administrator" title="Administrator"/>';
+	if($.inArray(UserFeatures.VIP, this.features))
+		icons += '<i class="icon-vip" title="VIP"/>';
+	if($.inArray(UserFeatures.MODERATOR, this.features))
+		icons += '<i class="icon-moderator" title="Moderator"/>';
+	if($.inArray(UserFeatures.SUBSCRIBER, this.features))
+		icons += '<i class="icon-subscriber" title="Subscriber"/>';
 	return icons;
 };
 
