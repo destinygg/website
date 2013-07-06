e978f7e761e5e4132229ea07ec236ead6f663273
Cene <cene@cene.co.za>
2013-07-06 20:29:54 +0200

    Cleaned up the chat icon css, there was some duplication

--- static/chat/js/scroll.mCustom.js
+++ static/chat/js/scroll.mCustom.js
@@ -1,3 +1,53 @@
+(function(w) {
+    "use strict";
+    // most browsers have an implementation
+    w.requestAnimationFrame = w.requestAnimationFrame ||
+            w.mozRequestAnimationFrame || w.webkitRequestAnimationFrame ||
+            w.msRequestAnimationFrame;
+    w.cancelAnimationFrame = w.cancelAnimationFrame ||
+            w.mozCancelAnimationFrame || w.webkitCancelAnimationFrame ||
+            w.msCancelAnimationFrame;
+ 
+    // polyfill, when necessary
+    if (!w.requestAnimationFrame) {
+        var aAnimQueue = [],
+            iRequestId = 0,
+            iIntervalId;
+ 
+        // create a mock requestAnimationFrame function
+        w.requestAnimationFrameLegacy = function(callback) {
+            aAnimQueue.push([++iRequestId, callback]);
+ 
+            if (!iIntervalId) {
+                iIntervalId = setInterval(function() {
+                    if (aAnimQueue.length) {
+                        aAnimQueue.shift()[1](+new Date());
+                    }
+                    else {
+                        // don't continue the interval, if unnecessary
+                        clearInterval(iIntervalId);
+                        iIntervalId = undefined;
+                    }
+                }, 1000 / 50);  // estimating support for 50 frames per second
+            }
+ 
+            return iRequestId;
+        };
+ 
+        // create a mock cancelAnimationFrame function
+        w.cancelAnimationFrameLegacy = function(requestId) {
+            // find the request ID and remove it
+            for (var i = 0, j = aAnimQueue.length; i < j; i += 1) {
+                if (aAnimQueue[i][0] === requestId) {
+                    aAnimQueue.splice(i, 1);
+                    return;
+                }
+            }
+        };
+    }
+})(window);
+
+
 (function($){
 	
 	destiny.fn.mCustomScrollbarPlugin = function(chat){
