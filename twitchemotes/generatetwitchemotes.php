2674beb031e32014e8f18062dd7091f4a96c5a0b
Peter SZTANOJEV <sztanpet@gmail.com>
2013-07-07 11:15:11 +0200

    new emotes

--- twitchemotes/generatetwitchemotes.php
+++ twitchemotes/generatetwitchemotes.php
@@ -1,20 +1,23 @@
 <?php
 error_reporting(E_ALL);
+
 $cssonly = ( @$argv[1] == 'css');
 $html = file_get_contents('http://twitchemotes.com/');
 
-$destination = 'img/twitch/%s.png';
-mkdir('img/twitch', 0775, true);
-mkdir('css', 0775, true);
+$twitchdestination = 'img/img/twitch-%s.png';
+@mkdir('img', 0775, true);
+@mkdir('css', 0775, true);
 preg_match_all('#<div class="span2">.*?<img src="([^"]*?)"/>.*>(.*?)</a></center><br/></div>#i', $html, $matches );
 
-$triggers = array();
-$css      = '
+$customemotes = array('Draven', 'INFESTINY', 'FIDGETLOL', 'Hhhehhehe');
+$triggers     = array();
+$css          = '
 /*
-	spritemapper.output_css = emoticons.css
+	spritemapper.output_css   = ../emoticons.css
+	spritemapper.output_image = ../emoticons.png
 	spritemapper.anneal_steps = 100
 */
-.twitch-emote {
+.chat-emote {
 	display: inline-block;
 	position: relative;
 	top: 10px;
@@ -22,19 +25,37 @@ $css      = '
 }
 ';
 $emotecss = "
-.twitch-emote.twitch-emote-%s {
+.chat-emote.chat-emote-%s {
 	width: %dpx;
 	height: %dpx;
 	margin-top: -%dpx;
-	background: url(../img/twitch/%s.png);
+	background: url(../img/%s);
 }
 ";
 
+foreach( $customemotes as $trigger ) {
+	
+	$path       = $trigger . '.png';
+	$filename   = 'img/' . $path;
+	$dimensions = getimagesize( $filename );
+	
+	$css .= sprintf(
+		$emotecss,
+		$trigger,
+		$dimensions[0],
+		$dimensions[1],
+		$dimensions[1],
+		$path
+	);
+	
+}
+
 foreach( $matches[1] as $key => $url ) {
 	
-	$trigger = $matches[2][ $key ];
+	$trigger  = $matches[2][ $key ];
 	preg_match('/-(\d+)x(\d+)\.png$/', $url, $dimensions );
-	$filename = sprintf( $destination, $trigger );
+	$filename = sprintf( $twitchdestination, $trigger );
+	$path     = sprintf('twitch-%s.png', $trigger );
 	
 	if ( !$cssonly ) {
 		$img = file_get_contents( $url );
@@ -47,10 +68,15 @@ foreach( $matches[1] as $key => $url ) {
 		$dimensions[1],
 		$dimensions[2],
 		$dimensions[2],
-		$trigger
+		$path
 	);
 	
 }
 
-file_put_contents('css/twitch.css', $css );
-echo implode("|", $matches[2]), "\n\nRun spritemapper css/twitch.css --anneal=100\n";
+file_put_contents('css/emoticons_unsprited.css', $css );
+echo
+	"/\\b(?:",
+	implode("|", array_merge($customemotes, $matches[2])),
+	")\\b/",
+	"\n\nRun spritemapper css/emoticons_unsprited.css --anneal=100\n"
+;
