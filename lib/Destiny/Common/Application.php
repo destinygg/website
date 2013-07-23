34d36c8fee6aada8878769a056e22b757231732e
Cene <cene@cene.co.za>
2013-07-23 17:50:16 +0200

    Too hasty... fix to config

--- lib/Destiny/Common/Application.php
+++ lib/Destiny/Common/Application.php
@@ -147,7 +147,7 @@ class Application extends Service {
 			$this->error ( Http::STATUS_ERROR, $e );
 		} catch ( \Exception $e ) {
 			$this->logger->critical ( $e->getMessage () );
-			if (Config::$a ['showExceptionMessages']) {
+			if (!Config::$a ['showExceptionMessages']) {
 				$this->error ( Http::STATUS_ERROR, new AppException ( 'Maximum over-rustle has been achieved' ) );
 			} else {
 				$this->error ( Http::STATUS_ERROR, $e->getMessage () );
