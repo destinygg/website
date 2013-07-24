95dc2b86cc11ae669eff3491dfea71a32346aaa1
Peter SZTANOJEV <sztanpet@gmail.com>
2013-07-24 20:04:20 +0200

    fix up the scheme i botched up modifying the protocol-less links code

--- static/chat/js/gui.js
+++ static/chat/js/gui.js
@@ -10,7 +10,7 @@ var linkregex;
 	    tlds     = "(?:AC|AD|AE|AERO|AF|AG|AI|AL|AM|AN|AO|AQ|AR|ARPA|AS|ASIA|AT|AU|AW|AX|AZ|BA|BB|BD|BE|BF|BG|BH|BI|BIZ|BJ|BM|BN|BO|BR|BS|BT|BV|BW|BY|BZ|CA|CAT|CC|CD|CF|CG|CH|CI|CK|CL|CM|CN|CO|COM|COOP|CR|CU|CV|CW|CX|CY|CZ|DE|DJ|DK|DM|DO|DZ|EC|EDU|EE|EG|ER|ES|ET|EU|FI|FJ|FK|FM|FO|FR|GA|GB|GD|GE|GF|GG|GH|GI|GL|GM|GN|GOV|GP|GQ|GR|GS|GT|GU|GW|GY|HK|HM|HN|HR|HT|HU|ID|IE|IL|IM|IN|INFO|INT|IO|IQ|IR|IS|IT|JE|JM|JO|JOBS|JP|KE|KG|KH|KI|KM|KN|KP|KR|KW|KY|KZ|LA|LB|LC|LI|LK|LR|LS|LT|LU|LV|LY|MA|MC|MD|ME|MG|MH|MIL|MK|ML|MM|MN|MO|MOBI|MP|MQ|MR|MS|MT|MU|MUSEUM|MV|MW|MX|MY|MZ|NA|NAME|NC|NE|NET|NF|NG|NI|NL|NO|NP|NR|NU|NZ|OM|ORG|PA|PE|PF|PG|PH|PK|PL|PM|PN|POST|PR|PRO|PS|PT|PW|PY|QA|RE|RO|RS|RU|RW|SA|SB|SC|SD|SE|SG|SH|SI|SJ|SK|SL|SM|SN|SO|SR|ST|SU|SV|SX|SY|SZ|TC|TD|TEL|TF|TG|TH|TJ|TK|TL|TM|TN|TO|TP|TR|TRAVEL|TT|TV|TW|TZ|UA|UG|UK|US|UY|UZ|VA|VC|VE|VG|VI|VN|VU|WF|WS|XXX|YE|YT|ZA|ZM|ZW)";
 	
 	linkregex = new RegExp(
-		"(?:^|[\\s([])" + //Match valid delimiters for multi URL lines
+		"(?:^|[\\s\\(\\[])" + //Match valid delimiters for multi URL lines
 		"(((?:https?|ftp):\\/\\/)?" + //Begin URL group, match and group protocol (if any).
 		"(?:[\\w]+(?::[" + urlchars + "]*)?@)?" + //Match basic auth user/password prefix
 		"(?:[\\w-]+\\.)+" + //match subdomains and domain
@@ -32,9 +32,7 @@ function urlReplaceCallback(match, url, scheme) {
 		return htmlencmap[c];
 	});
 	
-	if (!scheme)
-		scheme = 'http://';
-	
+	scheme = scheme ? '': 'http://';
 	return match.replace(url, '<a target="_blank" class="externallink" href="' + scheme + encodedUrl + '">' + encodedUrl + '</a>');
 }
 
