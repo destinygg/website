2c0eda47a29c841a7d6b0a2ddc0ad30616257925
Peter SZTANOJEV <sztanpet@gmail.com>
2013-07-26 10:05:59 +0200

    cleanup ipaddress handling

--- static/chat/js/gui.js
+++ static/chat/js/gui.js
@@ -7,22 +7,22 @@ var linkregex;
 (function() {
 	var urlchars = "\\w!\"#$%&'*+,-./:;<=>?@\\\\^`|~", //chars allowed in url path+auth params
 	    tlds     = "(?:AC|AD|AE|AERO|AF|AG|AI|AL|AM|AN|AO|AQ|AR|ARPA|AS|ASIA|AT|AU|AW|AX|AZ|BA|BB|BD|BE|BF|BG|BH|BI|BIZ|BJ|BM|BN|BO|BR|BS|BT|BV|BW|BY|BZ|CA|CAT|CC|CD|CF|CG|CH|CI|CK|CL|CM|CN|CO|COM|COOP|CR|CU|CV|CW|CX|CY|CZ|DE|DJ|DK|DM|DO|DZ|EC|EDU|EE|EG|ER|ES|ET|EU|FI|FJ|FK|FM|FO|FR|GA|GB|GD|GE|GF|GG|GH|GI|GL|GM|GN|GOV|GP|GQ|GR|GS|GT|GU|GW|GY|HK|HM|HN|HR|HT|HU|ID|IE|IL|IM|IN|INFO|INT|IO|IQ|IR|IS|IT|JE|JM|JO|JOBS|JP|KE|KG|KH|KI|KM|KN|KP|KR|KW|KY|KZ|LA|LB|LC|LI|LK|LR|LS|LT|LU|LV|LY|MA|MC|MD|ME|MG|MH|MIL|MK|ML|MM|MN|MO|MOBI|MP|MQ|MR|MS|MT|MU|MUSEUM|MV|MW|MX|MY|MZ|NA|NAME|NC|NE|NET|NF|NG|NI|NL|NO|NP|NR|NU|NZ|OM|ORG|PA|PE|PF|PG|PH|PK|PL|PM|PN|POST|PR|PRO|PS|PT|PW|PY|QA|RE|RO|RS|RU|RW|SA|SB|SC|SD|SE|SG|SH|SI|SJ|SK|SL|SM|SN|SO|SR|ST|SU|SV|SX|SY|SZ|TC|TD|TEL|TF|TG|TH|TJ|TK|TL|TM|TN|TO|TP|TR|TRAVEL|TT|TV|TW|TZ|UA|UG|UK|US|UY|UZ|VA|VC|VE|VG|VI|VN|VU|WF|WS|XXX|YE|YT|ZA|ZM|ZW)",
-		ipaddr = "(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9‌]{2}|2[0-4][0-9]|25[0-5])";
+	    ipaddr   = "(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])";
 	
 	linkregex = new RegExp(
 		"(?:^|[\\s\\(\\[])" + //Match valid delimiters for multi URL lines
 		"(((?:https?|ftp):\\/\\/)?" + //Begin URL group, match and group protocol (if any).
 		"(?:[\\w]+(?::[" + urlchars + "]*)?@)?" + //Match basic auth user/password prefix
-		"(" + ipaddr + "|(?:"+ // match an ip address or domain
+		"(?:" + ipaddr + "|(?:"+ // match an ip address or domain
 		"(?:[\\w-]+\\.)+" + //match subdomains and domain
 		tlds + //match valid+accepted TLDs, we don't care about punycode
-		"))(\\/[" + urlchars + "]*)?)" + //match path and query, END URL group
+		"))(?:\\/[" + urlchars + "]*)?)" + //match path and query, END URL group
 		"(?=$|[\\s\\)\\]])", // Look ahead for valid delimiters
 		"gim"
 	);
 })();
 
-function urlReplaceCallback(match, url, scheme, address, path, offset, message) {
+function urlReplaceCallback(match, url, scheme) {
 	var encodedUrl = url.replace(/[&"'<>]/g, function(c) {
 		var htmlencmap = {
 			"&": "&amp;",
