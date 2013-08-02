154a10ceae13b760e2a929ee197d4052a4f2335d
Cene <cene@cene.co.za>
2013-08-02 13:10:24 +0200

    Version bump, updated youtube links

--- public/tpl/errors/top.php
+++ public/tpl/errors/top.php
@@ -6,7 +6,7 @@
 						<li><a title="Home Page" href="/"><i class="icon-home icon-white subtle"></i></a></li>
 						<li><a title="Blog @ destiny.gg" href="http://blog.destiny.gg">Blog</a></li>
 						<li><a title="twitter.com" href="https://twitter.com/Steven_Bonnell/">Twitter</a></li>
-						<li><a title="youtube.com" href="http://www.youtube.com/user/StevenBonnell">Youtube</a></li>
+						<li><a title="youtube.com" href="http://www.youtube.com/user/Destiny">Youtube</a></li>
 						<li><a title="reddit.com" href="http://www.reddit.com/r/Destiny/">Reddit</a></li>
 						<li><a title="facebook.com" href="https://www.facebook.com/Steven.Bonnell.II">Facebook</a></li>
 						<li class="divider-vertical"></li>
