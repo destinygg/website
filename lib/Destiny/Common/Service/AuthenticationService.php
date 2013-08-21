04b55615d1ef797c71f9727f0ba30931491e93c4
Cene <cene@cene.co.za>
2013-08-21 19:39:05 +0200

    Removed username from auth check all together

--- lib/Destiny/Common/Service/AuthenticationService.php
+++ lib/Destiny/Common/Service/AuthenticationService.php
@@ -182,7 +182,7 @@ class AuthenticationService extends Service {
 	 * @throws AppException
 	 */
 	private function validateAuthCredentials(array $authCreds) {
-		if (! isset ( $authCreds ['authId'] ) || ! isset ( $authCreds ['username'] ) || (isset($authCreds ['username']) && empty ( $authCreds ['username'] )) || ! isset ( $authCreds ['email'] ) || ! isset ( $authCreds ['authCode'] ) || ! isset ( $authCreds ['authProvider'] )) {
+		if (! isset ( $authCreds ['authId'] ) || ! isset ( $authCreds ['username'] ) || ! isset ( $authCreds ['email'] ) || ! isset ( $authCreds ['authCode'] ) || ! isset ( $authCreds ['authProvider'] )) {
 			Application::instance ()->getLogger ()->error ( sprintf ( 'Error validating auth credentials %s', var_export ( $authCreds, true ) ) );
 			throw new AppException ( 'Invalid auth credentials' );
 		}
