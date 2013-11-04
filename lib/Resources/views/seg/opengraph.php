14b72de159ecb771a7ed2de6eb4c46ab851c4f41
Cene <cene@cene.co.za>
2013-11-04 14:25:37 +0200

    Replaced the old website og:image with a new image made the video a rounded off size (part of an issue with facebook)

--- lib/Resources/views/seg/opengraph.php
+++ lib/Resources/views/seg/opengraph.php
@@ -5,14 +5,14 @@ $arr = array (
 	"og:site_name" => Config::$a ['meta'] ['shortName'],
 	"og:title" => Config::$a ['meta'] ['title'],
 	"og:description" => Config::$a['meta']['description'],
-	"og:image" => Config::cdn() . '/web/img/destinyspash600x600.png',
+	"og:image" => Config::cdn() . '/web/img/destinygg.png',
 	"og:url" => Http::getBaseUrl(),
 	"og:type" => "video.other",
 	"og:video" => Config::$a['meta']['video'],
 	"og:video:secure_url" => Config::$a['meta']['videoSecureUrl'],
 	"og:video:type" => "application/x-shockwave-flash",
-	"og:video:height" => "259",
-	"og:video:width" => "398",
+	"og:video:height" => "260",
+	"og:video:width" => "340",
 );
 foreach ($arr as $k=>$v){
 	echo '<meta property="'. $k .'" content="'. $v .'" />' . PHP_EOL;
