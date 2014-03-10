844bd41693ed16645adad7d0895aad8c9c04c01e
Cene <cene@cene.co.za>
2014-03-10 23:28:02 +0200

    Added schedule link to top Remove user agreement from footer (still seen during sign-up / subscription) Remove contact link from top, added to footer

--- lib/Resources/views/seg/top.php
+++ lib/Resources/views/seg/top.php
@@ -9,12 +9,12 @@ use Destiny\Common\Session;
 			<div class="nav-collapse collapse">
 				<a class="brand pull-left" href="/">Destiny.gg</a>
 				<ul id="top-left-nav" class="nav">
-					<li><a title="Contact me by email" target="_blank" href="mailto:steven.bonnell.ii@gmail.com">@Contact</a></li>
 					<li><a title="Blog @ destiny.gg" href="http://blog.destiny.gg">Blog</a></li>
 					<li><a title="twitter.com" href="https://twitter.com/Steven_Bonnell/">Twitter</a></li>
 					<li><a title="youtube.com" href="http://www.youtube.com/user/Destiny">Youtube</a></li>
 					<li><a title="reddit.com" href="http://www.reddit.com/r/Destiny/">Reddit</a></li>
 					<li><a title="facebook.com" href="https://www.facebook.com/Steven.Bonnell.II">Facebook</a></li>
+					<li><a title="Schedule" href="https://www.google.com/calendar/embed?src=i54j4cu9pl4270asok3mqgdrhk%40group.calendar.google.com">Schedule</a></li>
 					<?if(!Session::hasRole(UserRole::SUBSCRIBER)):?>
 					<li class="divider-vertical"></li>
 					<li class="subscribe"><a href="/subscribe" rel="subscribe" title="Get your own destiny.gg subscription"><span>Subscribe</span></a></li>
