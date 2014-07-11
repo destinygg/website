816331d381a557877f36fef67b689adcacc85e2d
Cene <cene@cene.co.za>
2014-07-11 16:29:30 +0200

    Reduced the outbound page to simple white text and 'instant' redirect

--- lib/Resources/views/outbound.php
+++ lib/Resources/views/outbound.php
@@ -8,48 +8,19 @@ use Destiny\Common\Config;
 <head>
 <title><?=Tpl::title($model->title)?></title>
 <meta charset="utf-8">
-<meta name="description" content="<?=Config::$a['meta']['description']?>">
-<meta name="keywords" content="<?=Config::$a['meta']['keywords']?>">
-<meta name="author" content="<?=Config::$a['meta']['author']?>">
-<?php include Tpl::file('seg/opengraph.php') ?>
-<?php include Tpl::file('seg/commontop.php') ?>
 <?php include Tpl::file('seg/google.tracker.php') ?>
+<script>
+var url = '<?= Tpl::out($model->url) ?>';
+_gaq.push(['_trackEvent', 'outbound', 'redirect', url]);
+_gaq.push(function(){
+    window.location.replace(url);
+});
+</script>
 </head>
-<body id="home">
-
-    <?php include Tpl::file('seg/headerband.php') ?>
-
-    <section class="container">
-        <h1 class="title">You are being redirected <small id="timeleft">in 3 seconds</small></h1>
-        <div class="content content-dark clearfix">
-            <div class="ds-block">
-                <a href="<?= Tpl::out($model->url) ?>">
-                    <span class="glyphicon glyphicon-share"></span>
-                </a>
-                <?= Tpl::out($model->url) ?>
-            </div>
-        </div>
-    </section>
-
-    <?php include Tpl::file('seg/commonbottom.php') ?>
-
-    <script>
-    var cnt = 3,
-        url = '<?= Tpl::out($model->url) ?>';
-    _gaq.push(['_trackEvent', 'outbound', 'redirect', url]);
-    _gaq.push(function() {
-        var interval = setInterval(function() {
-            cnt--;
-            if (cnt == 0) {
-                $('#timeleft').text('now ...');
-                window.location.replace(url);
-                clearInterval(interval);
-            }else{
-                $('#timeleft').text('in '+ cnt +' seconds');
-            }
-        }, 1000);
-    });
-    </script>
-
+<body>
+    <p>Please wait a few while we redirect you &hellip;</p>
+    <noscript>
+       <p>No javascript present >:( &hellip; Click the link <a rel="nofollow" href="<?= Tpl::out($model->url) ?>"><?= Tpl::out($model->url) ?></a></p>
+    </noscript>
 </body>
 </html>
