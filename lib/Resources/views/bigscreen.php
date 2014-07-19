87e491a5183339bad67a7cabacb03be813ecbda9
Cene <cene@cene.co.za>
2014-07-19 18:01:38 +0200

    version bump reworked the css and dom on /bigscreen and /screen (now uses calc css3 methods) Added ability to resize the chat on bigscreen (also remembers it) Converted ui.js to x4 spaces instead of x2 (... I have to)

--- lib/Resources/views/bigscreen.php
+++ lib/Resources/views/bigscreen.php
@@ -13,74 +13,68 @@ use Destiny\Common\Config;
 <?php include Tpl::file('seg/commontop.php') ?>
 <?php include Tpl::file('seg/google.tracker.php') ?>
 </head>
-<body id="bigscreen" class="thin" style="overflow: hidden;">
-  <div class="page-wrap clearfix">
-    
-    <?php include Tpl::file('seg/top.php') ?>
-  
-    <div id="page-content" class="container clearfix" style="padding:0;">
-
-      <div id="stream-panel" class="pull-left">
-        <div>
-
-          <div class="panelheader clearfix">
-            <div class="toolgroup clearfix">
-              <div class="pull-left channel-stat game">
-                <?php if(!isset($model->streamInfo['stream']) || empty($model->streamInfo['stream'])): ?>
-                <span class="glyphicon glyphicon-time"></span>
-                <span>
-                <?php if(isset($model->streamInfo['lastbroadcast'])): ?>
-                Last broadcast ended <?=Date::getElapsedTime(Date::getDateTime($model->streamInfo['lastbroadcast']))?>
-                <?php endif; ?>
-                </span>
-                <?php else: ?>
-                <span class="glyphicon glyphicon-time"></span> <span>Started <?=Date::getElapsedTime(Date::getDateTime($model->streamInfo['lastbroadcast']))?></span>
-                <?php if(isset($model->streamInfo['stream']) && intval($model->streamInfo['stream']['channel']['delay']) > 1): ?>
-                - <?=(intval($model->streamInfo['stream']['channel']['delay'])/60)?>m delay
-                <?php endif; ?>
-              <?php endif; ?>
-              </div>
-              <div class="pull-right channel-stat" style="text-align:right;"><?=(isset($model->streamInfo['status'])) ? Tpl::out($model->streamInfo['status']) : ''?></div>
-            </div>
-          </div>
-
-          <div class="stream-element-wrap">
-            <div class="stream-overlay to-botright"></div>
-            <div class="stream-overlay to-botleft"></div>
-            <div class="stream-overlay to-main"></div>
-            <div class="stream-overlay fsbtn" title="Fullscreen"></div>
-            <object class="stream-element" type="application/x-shockwave-flash" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=destiny" height="100%" width="100%">
-              <param name="allowFullScreen" value="true">
-              <param name="allowScriptAccess" value="always">
-              <param name="allowNetworking" value="all">
-              <param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf">
-              <param name="flashvars" value="hostname=www.twitch.tv&amp;channel=<?=Config::$a['twitch']['user']?>&amp;auto_play=true&amp">
-            </object>
-          </div>
-        </div>
+<body id="bigscreen" class="thin">
         
-      </div>
+    <?php include Tpl::file('seg/top.php') ?>
 
-      <div id="chat-panel" class="pull-right">
-        <div>
-          <div class="panelheader clearfix">
-            <div class="toolgroup clearfix">
-              <div id="chat-panel-tools">
-                <a title="Refresh" id="refresh" class="pull-left"><span class="glyphicon glyphicon-refresh"></span></a>
-                <a title="Close" id="close" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
-                <a title="Popout" id="popout" class="pull-right"><span class="glyphicon glyphicon-share"></span></a>
-              </div>
+    <div id="page-content" class="container clearfix">
+
+        <div id="stream-panel">
+
+            <div class="panelheader clearfix">
+                <div class="toolgroup clearfix">
+                    <div class="pull-left channel-stat game">
+                    <?php if(!isset($model->streamInfo['stream']) || empty($model->streamInfo['stream'])): ?>
+                        <span class="glyphicon glyphicon-time"></span>
+                        <span>
+                        <?php if(isset($model->streamInfo['lastbroadcast'])): ?>
+                        Last broadcast ended <?=Date::getElapsedTime(Date::getDateTime($model->streamInfo['lastbroadcast']))?>
+                        <?php endif; ?>
+                        </span>
+                        <?php else: ?>
+                        <span class="glyphicon glyphicon-time"></span> <span>Started <?=Date::getElapsedTime(Date::getDateTime($model->streamInfo['lastbroadcast']))?></span>
+                        <?php if(isset($model->streamInfo['stream']) && intval($model->streamInfo['stream']['channel']['delay']) > 1): ?>
+                        - <?=(intval($model->streamInfo['stream']['channel']['delay'])/60)?>m delay
+                        <?php endif; ?>
+                    <?php endif; ?>
+                    </div>
+                    <div class="pull-right channel-stat" style="text-align:right;"><?=(isset($model->streamInfo['status'])) ? Tpl::out($model->streamInfo['status']) : ''?></div>
+                </div>
             </div>
-          </div>
-          <iframe id="chat-frame" class="stream-element" style="border:none;" seamless="seamless" src="/embed/chat"></iframe>
+
+            <div id="stream-wrap">
+                <div class="stream-overlay to-botright"></div>
+                <div class="stream-overlay to-botleft"></div>
+                <div class="stream-overlay to-main"></div>
+                <div class="stream-overlay fsbtn" title="Fullscreen"></div>
+                <object class="stream-element" type="application/x-shockwave-flash" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=destiny" height="100%" width="100%">
+                    <param name="allowFullScreen" value="true">
+                    <param name="allowScriptAccess" value="always">
+                    <param name="allowNetworking" value="all">
+                    <param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf">
+                    <param name="flashvars" value="hostname=www.twitch.tv&amp;channel=<?=Config::$a['twitch']['user']?>&amp;auto_play=true">
+                </object>
+            </div>
+            
         </div>
-      </div>
+
+        <div id="chat-panel">
+            <div id="chat-panel-resize-bar"></div>
+            <div class="panelheader clearfix">
+                <div class="toolgroup clearfix">
+                    <div id="chat-panel-tools">
+                        <a title="Refresh" id="refresh" class="pull-left"><span class="glyphicon glyphicon-refresh"></span></a>
+                        <a title="Close" id="close" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+                        <a title="Popout" id="popout" class="pull-right"><span class="glyphicon glyphicon-share"></span></a>
+                    </div>
+                </div>
+            </div>
+            <iframe id="chat-frame" class="stream-element" style="border:none; width: 100%;" seamless="seamless" src="/embed/chat"></iframe>
+        </div>
 
     </div>
-  
-  </div>
-  
-  <?php include Tpl::file('seg/commonbottom.php') ?>
-  
+    
+    <?php include Tpl::file('seg/commonbottom.php') ?>
+    
 </body>
 </html>
--- lib/Resources/views/seg/panel.twitch.php
+++ lib/Resources/views/seg/panel.twitch.php
@@ -3,46 +3,44 @@ use Destiny\Common\Utils\Date;
 use Destiny\Common\Config;
 ?>
 <section id="twitchpanel" class="container split-view" data-youtube-user="<?=Config::$a['youtube']['user']?>" data-youtube-playlist="<?=Config::$a['youtube']['playlistId']?>" data-video-embed="http://www.twitch.tv/<?=Config::$a['twitch']['user']?>/popout" data-chat-embed="/embed/chat">
-	<div class="content content-dark">
-		<div class="panelheader clearfix">
-			<div class="toolgroup clearfix">
-				<div class="pull-left channel-stat game">
-				<?php if(!empty($model->streamInfo)): ?>
-					<?php if(!isset($model->streamInfo['stream']) || empty($model->streamInfo['stream'])): ?>
-					<span class="glyphicon glyphicon-time"></span> 
-					<span>Last broadcast ended <?=Date::getElapsedTime(Date::getDateTime($model->streamInfo['lastbroadcast']))?></span>
-					<?php else: ?>
-					<span class="glyphicon glyphicon-time"></span> 
-					<span>Started <?=Date::getElapsedTime(Date::getDateTime($model->streamInfo['lastbroadcast']))?></span>
-					<?php if(intval($model->streamInfo['stream']['channel']['delay']) > 1): ?>
-					- <?=(intval($model->streamInfo['stream']['channel']['delay'])/60)?>m delay
-					<?php endif; ?>
-					<?php endif; ?>
-				<?php endif; ?>
-				</div>
-				<div class="btn-group pull-right">
-					<a id="popoutchat" title="Pop-out chat" href="/embed/chat" class="btn btn-xs btn-link">Pop-out chat</a>
-					<a id="popoutvideo" title="Pop-out video" href="http://www.twitch.tv/destiny/popout" class="btn btn-xs btn-link">Pop-out stream</a>
-				</div>
-			</div>
-		</div>
-		<div class="clearfix" id="stream-element-wrap">
-			<div id="player-embed" class="stream-element-wrap pull-left">
-				<div class="stream-overlay to-botright"></div>
-				<div class="stream-overlay to-botleft"></div>
-				<div class="stream-overlay to-main"></div>
-				<div class="stream-overlay fsbtn" title="Fullscreen"></div>
-				<object class="stream-element" type="application/x-shockwave-flash" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=<?=Config::$a['twitch']['user']?>" height="100%" width="100%">
-					<param name="allowFullScreen" value="true">
-					<param name="allowScriptAccess" value="always">
-					<param name="allowNetworking" value="all">
-					<param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf">
-					<param name="flashvars" value="hostname=www.twitch.tv&amp;channel=<?=Config::$a['twitch']['user']?>&amp;auto_play=true&amp">
-				</object>
-			</div>
-			<div id="chat-embed" class="stream-element-wrap pull-left">
-				<iframe class="stream-element" style="border:none;" seamless="seamless" src="/embed/chat"></iframe>
-			</div>
-		</div>
-	</div>
+    <div class="content content-dark">
+        <div class="panelheader clearfix">
+            <div class="toolgroup clearfix">
+                <div class="pull-left channel-stat game">
+                <?php if(!empty($model->streamInfo)): ?>
+                    <?php if(!isset($model->streamInfo['stream']) || empty($model->streamInfo['stream'])): ?>
+                    <span class="glyphicon glyphicon-time"></span> 
+                    <span>Last broadcast ended <?=Date::getElapsedTime(Date::getDateTime($model->streamInfo['lastbroadcast']))?></span>
+                    <?php else: ?>
+                    <span class="glyphicon glyphicon-time"></span> 
+                    <span>Started <?=Date::getElapsedTime(Date::getDateTime($model->streamInfo['lastbroadcast']))?></span>
+                    <?php if(intval($model->streamInfo['stream']['channel']['delay']) > 1): ?>
+                    - <?=(intval($model->streamInfo['stream']['channel']['delay'])/60)?>m delay
+                    <?php endif; ?>
+                    <?php endif; ?>
+                <?php endif; ?>
+                </div>
+                <div class="btn-group pull-right">
+                    <a id="popoutchat" title="Pop-out chat" href="/embed/chat" class="btn btn-xs btn-link">Pop-out chat</a>
+                    <a id="popoutvideo" title="Pop-out video" href="http://www.twitch.tv/destiny/popout" class="btn btn-xs btn-link">Pop-out stream</a>
+                </div>
+            </div>
+        </div>
+        <div id="player-embed">
+            <div class="stream-overlay to-botright"></div>
+            <div class="stream-overlay to-botleft"></div>
+            <div class="stream-overlay to-main"></div>
+            <div class="stream-overlay fsbtn" title="Fullscreen"></div>
+            <object class="stream-element" type="application/x-shockwave-flash" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=<?=Config::$a['twitch']['user']?>" height="100%" width="100%">
+                <param name="allowFullScreen" value="true">
+                <param name="allowScriptAccess" value="always">
+                <param name="allowNetworking" value="all">
+                <param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf">
+                <param name="flashvars" value="hostname=www.twitch.tv&amp;channel=<?=Config::$a['twitch']['user']?>&amp;auto_play=true&amp">
+            </object>
+        </div>
+        <div id="chat-embed">
+            <iframe class="stream-element" style="border:none;" seamless="seamless" src="/embed/chat"></iframe>
+        </div>
+    </div>
 </section>
