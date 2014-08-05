0a6c9cb1b58788f1c28e1ec8cacb1ed39608ae0f
Cene <cene@cene.co.za>
2014-08-05 20:12:44 +0200

    Added a 3.5 second timer, for the outbound.php .. for those who block ga.js.. (it should always take less that 3.5 seconds for the tracking event to fire)

--- lib/Resources/views/outbound.php
+++ lib/Resources/views/outbound.php
@@ -8,17 +8,27 @@ use Destiny\Common\Config;
 <head>
 <title><?=Tpl::title($model->title)?></title>
 <meta charset="utf-8">
+<script src="<?=Config::cdnvf('1.1.0')?>/vendor/libs.min.js"></script>
 <?php include Tpl::file('seg/google.tracker.php') ?>
 <script>
+
+// Make sure the user is redirected (for those who block ga.js)
+var to = window.setTimeout(function(){
+    window.location.replace(url);
+}, 3500);
+
+// Normal analytics
 var url = '<?= Tpl::out($model->url) ?>';
 _gaq.push(['_trackEvent', 'outbound', 'redirect', url]);
 _gaq.push(function(){
+    window.clearTimeout(to);
     window.location.replace(url);
 });
+
 </script>
 </head>
 <body>
-    <p>Please wait while we redirect you &hellip;</p>
+    <p>Please wait while we redirect you to <a rel="nofollow" href="<?= Tpl::out($model->url) ?>"><?= Tpl::out($model->url) ?></a> &hellip;</p>
     <noscript>
        <p>No javascript present >:( &hellip; Click the link <a rel="nofollow" href="<?= Tpl::out($model->url) ?>"><?= Tpl::out($model->url) ?></a></p>
     </noscript>
