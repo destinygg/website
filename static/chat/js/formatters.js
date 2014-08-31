208f0454445508eb184bebe73dac6088b3fdd1e7
Peter SZTANOJEV <sztanpet@gmail.com>
2014-09-01 00:04:46 +0200

    even more natural emote matching, use positive lookaheads so that people dont need two spaces between emotes

--- static/chat/js/formatters.js
+++ static/chat/js/formatters.js
@@ -25,13 +25,13 @@
 	
 	// Emote line formatter
 	destiny.fn.EmoteFormatter = function(chat){
-		this.emoteregex = new RegExp('(^|\\s)('+chat.emoticons.join('|')+')($|[\\s,\\.\\?!])');
-		this.gemoteregex = new RegExp('(^|\\s)('+chat.emoticons.join('|')+')($|[\\s,\\.\\?!])', 'gm');
+		this.emoteregex = new RegExp('(^|[\\s,\\.\\?!])('+chat.emoticons.join('|')+')(?=$|[\\s,\\.\\?!])');
+		this.gemoteregex = new RegExp('(^|[\\s,\\.\\?!])('+chat.emoticons.join('|')+')(?=$|[\\s,\\.\\?!])', 'gm');
 		return this;
 	};
 	destiny.fn.EmoteFormatter.prototype.format = function(str, user){
 		var emoteregex = (user && ((user.features || []).length > 0)) ? this.gemoteregex:this.emoteregex;
-		return str.replace(emoteregex, '$1<div title="$2" class="chat-emote chat-emote-$2"></div>$3');
+		return str.replace(emoteregex, '$1<div title="$2" class="chat-emote chat-emote-$2"></div>');
 	};
 	
 	// URL line formatter
