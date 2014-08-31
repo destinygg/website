728b77b6502488c67dd41585a6f8560271ca218d
Peter SZTANOJEV <sztanpet@gmail.com>
2014-09-01 01:13:15 +0200

    seems like the bug is found

--- static/chat/js/gui.js
+++ static/chat/js/gui.js
@@ -658,7 +658,8 @@
         },
         
         addBroadcastUI: function(message){
-            console.log(message);
+
+            if ( typeof( message ) == 'string' ) return;
             if (message.data.substring(0, 9) == 'redirect:') return;
             // Dont show the broadcast if the user has already dismissed it
             if(this.broadcastdismiss != null && moment.utc(message.timestamp).unix() < this.broadcastdismiss){
