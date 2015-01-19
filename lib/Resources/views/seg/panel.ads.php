2e1ee7d079f39c8da68e58332866a77096f3fbef
Cene <cene@cene.co.za>
2015-01-20 00:01:02 +0200

    Removed http: from ting and google ads.

--- lib/Resources/views/seg/panel.ads.php
+++ lib/Resources/views/seg/panel.ads.php
@@ -16,8 +16,8 @@ use Destiny\Common\Config;
     <div id="dc-donate-wrap">
         <div id="ting-block" class="dc-donate-blk">
             <div class="dc-donate-inner">
-                <img height="1" width="1" src="http://ting.7eer.net/i/72409/87559/2020" border="0" style="position: absolute; top:0; left:0;" />
-                <a href="http://ting.7eer.net/c/72409/87559/2020"><img src="http://adn.impactradius.com/display-ad/2020-87559" border="0" alt="" width="250" height="250"/></a>
+                <img height="1" width="1" src="//ting.7eer.net/i/72409/87559/2020" border="0" style="position: absolute; top:0; left:0;" />
+                <a href="http://ting.7eer.net/c/72409/87559/2020"><img src="//adn.impactradius.com/display-ad/2020-87559" border="0" alt="" width="250" height="250"/></a>
             </div>
         </div>
         <div id="donate-block" class="dc-donate-blk visible-lg">
@@ -36,7 +36,7 @@ use Destiny\Common\Config;
                     <?= (is_int ( $v )) ? "$k = $v;\r\n" : "$k = \"$v\";"; ?>
                 <?php endforeach; ?>
                 </script>
-                <script src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+                <script src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>
             </div>
         </div>
     </div>
