5366cbfe33efa4cd0e6234bbd0003d11a444c258
Destiny Dori <destiny@dori.destiny.gg>
2015-04-25 16:35:50 -0400

    actually use the correct stats socket path

--- config/updateocsp.sh
+++ config/updateocsp.sh
@@ -3,4 +3,4 @@ openssl ocsp -noverify -issuer /etc/ssl/certs/COMODORSADomainValidationSecureSer
     -cert /etc/ssl/certs/destiny.gg.crt -url "http://ocsp.comodoca.com" \
     -respout /etc/ssl/certs/destiny.gg.wildcard_combined.crt.ocsp > /dev/null && \
 echo "set ssl ocsp-response $(/usr/bin/base64 -w 10000 /etc/ssl/certs/destiny.gg.wildcard_combined.crt.ocsp)" | \
-/usr/bin/socat stdio /var/run/haproxy.stats
+/usr/bin/socat stdio /var/lib/haproxy/stats > /dev/null
