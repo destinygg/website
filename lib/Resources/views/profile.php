77f56ff5c26bad2ea03790804c7d4cba2abb6ba8
Peter SZTANOJEV <sztanpet@gmail.com>
2015-09-13 22:10:05 +0200

    add some information to the form when setting the minecraft name

--- lib/Resources/views/profile.php
+++ lib/Resources/views/profile.php
@@ -166,7 +166,9 @@ use Destiny\Commerce\SubscriptionStatus;
               </div>
               
               <div class="form-group">
-                <label>Minecraft name:</label>
+                <label>Minecraft name:
+                <br><small>For the minecraft server details, ask in chat.</small>
+                </label>
                 <input class="form-control" type="text" name="minecraftname" value="<?=Tpl::out($model->user['minecraftname'])?>" placeholder="Minecraft name" />
               </div>
               
