a97acefd9c87e312a9f93aebff8d2694ccf51b09
Cene <cene@cene.co.za>
2016-07-20 01:31:02 +0200

    UI update on amazon page.

--- lib/Resources/views/amazon.php
+++ lib/Resources/views/amazon.php
@@ -12,43 +12,35 @@ use Destiny\Common\Config;
 <?php include Tpl::file('seg/commontop.php') ?>
 <?php include Tpl::file('seg/google.tracker.php') ?>
 </head>
-<body id="amazon">
+<body id="amazon" class="no-brand">
     <div id="page-wrap">
+
         <?php include Tpl::file('seg/top.php') ?>
         <?php include Tpl::file('seg/headerband.php') ?>
+
         <div class="container">
             <div class="row">
-                <div class="col-md-4 amazon-referral">
-                    <a href="http://www.amazon.ca/?tag=destiny0f7a-20">
-                        <h2 style="font-weight: normal;">Canada</h2>
-                        <img alt="amazon.ca" width="250" height="65" src="<?=Config::cdn()?>/web/img/amazon.ca.jpg" />
-                    </a>
+                <div style="text-align: center;">
+                    <div style="margin-top: 60px; margin-bottom: 40px;">
+                        <a href="http://www.amazon.com/?tag=des000-20"><img alt="amazon.com" src="<?=Config::cdn()?>/web/img/amazon.png" /></a>
+                    </div>
+                    <div>
+                        <a href="http://www.amazon.com/?tag=des000-20">US</a>
+                        <span style="padding:0 5px;">&bull;</span>
+                        <a href="http://www.amazon.ca/?tag=destiny0f7a-20">Canada</a>
+                        <span style="padding:0 5px;">&bull;</span>
+                        <a href="http://www.amazon.co.uk/?tag=destiny0f7-21">United Kingdom</a>
+                        <span style="padding:0 5px;">&bull;</span>
+                        <a href="http://www.amazon.de/?tag=destiny0f-21">Germany</a>
+                        <span style="padding:0 5px;">&bull;</span>
+                        <a href="http://www.amazon.com/?tag=des000-20">Other</a>
+                    </div>
                 </div>
-                <div class="col-md-4 amazon-referral">
-                    <a href="http://www.amazon.com/?tag=des000-20">
-                        <h2>US &amp; Other</h2>
-                        <img alt="amazon.com" width="250" height="65" src="<?=Config::cdn()?>/web/img/amazon.com.jpg" />
-                    </a>
-                </div>
-                <div class="col-md-4 amazon-referral">
-                    <a href="http://www.amazon.co.uk/?tag=destiny0f7-21">
-                        <h2>United Kingdom</h2>
-                        <img alt="amazon.co.uk" width="250" height="65" src="<?=Config::cdn()?>/web/img/amazon.co.uk.jpg" />
-                    </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-4 amazon-referral">
-                    <a href="http://www.amazon.de/?tag=destiny0f-21">
-                        <h2>Germany</h2>
-                        <img alt="amazon.de" width="250" height="65" src="<?=Config::cdn()?>/web/img/amazon.de.jpg" />
-                    </a>
-                </div>
-                <div class="col-md-4"></div>
             </div>
         </div>
+
     </div>
+
     <?php include Tpl::file('seg/foot.php') ?>
     <?php include Tpl::file('seg/commonbottom.php') ?>
 </body>
