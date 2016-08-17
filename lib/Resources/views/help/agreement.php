571c1d73569a605d6225c7d028e9cafdd2171532
Cene <cene@cene.co.za>
2016-08-18 00:13:51 +0200

    Added some rules and acknowledgments to the agreement page.

--- lib/Resources/views/help/agreement.php
+++ lib/Resources/views/help/agreement.php
@@ -25,7 +25,31 @@ use Destiny\Common\Utils\Tpl;
             </h1>
             <div class="content content-dark clearfix">
                 <div class="ds-block">
-                    <p>There is no user agreement.</p>
+
+                    <h3>Your Acknowledgement</h3>
+                    <p>Any data stored in this website has the potential to be accessed by a 3rd party through some unforeseen security breach. We do our best to maintain security.</p>
+
+                    <h3>Cookies</h3>
+                    <p>When you have a logged in session with the website, a cookie is used to track who you are; it contains a simple unique string used to identify your requests. There is no way around this.</p>
+                    <p>If a 3rd person gets a hold of this cookie, they can imitate your user, as well as gain access to your account. It is as secure as your browser is.</p>
+
+                    <h3>Local Storage</h3>
+                    <p>We use local storage to hold data that we deem of low security. Things like your chat ignore list.</p>
+                    <p>These are as secure as you make your browser and we are not responsible for its privacy.</p>
+
+                    <h3>IP Addresses</h3>
+                    <p>Your IP address is recorded for a time period with your session; and is accessible to admins of the website.</p>
+
+                    <h3>Reserved Rights</h3>
+                    <p>We reserve the right to block or delete your account by any means we feel appropriate.</p>
+                    <p>We reserve the right to not issue refunds on subscriptions or donations when deemed appropriate.</p>
+                    <p>We reserve the right to respond or not respond, within whatever time frame we can afford, to questions and or bug reports regarding this website.</p>
+
+                    <h3>Rules</h3>
+                    <p>Do not attempt to gain access to data or user accounts that are not yours.</p>
+                    <p>Do not create user profiles using automated code.</p>
+                    <p>Do not try to impersonate anyone.</p>
+
                 </div>
             </div>
         </section>
