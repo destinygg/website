d95c67b8edb1c5c79c713bb5364c3c2d7213d891
Cene <cene@cene.co.za>
2016-10-11 23:10:05 +0200

    Removed unrequired destiny.init inline script. Added weird self src lookup to the destiny.js -- temp measure, shouldnt need cdn location at all?

--- lib/Resources/views/seg/commonbottom.php
+++ lib/Resources/views/seg/commonbottom.php
@@ -3,4 +3,3 @@ use Destiny\Common\Config;
 ?>
 <script src="<?=Config::cdnv()?>/vendor/libs.min.js"></script>
 <script src="<?=Config::cdnv()?>/web/js/destiny.min.js"></script>
-<script>destiny.init({cdn:'<?=Config::cdn()?>'});</script>
--- static/web/js/destiny.js
+++ static/web/js/destiny.js
@@ -1,10 +1,11 @@
 destiny = {
-    cdn:        '',
+    cdn: (function() {
+            var scripts = document.getElementsByTagName('script'),
+                src = scripts[scripts.length - 1].src;
+            return src.substring(0, src.indexOf('/web/js/destiny.min.js'));
+        })(),
     token:      '',
     baseUrl:    '/',
     timeout:    15000,
     fn:         {}
-};
-destiny.init = function(args){
-    $.extend(destiny, args);
 };
