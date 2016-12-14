6aa8178245808b7448187de7dcc1879db31b556f
Cene <cene@cene.co.za>
2016-12-14 02:20:43 +0200

    Added google verification meta tag to commontop.php

--- config/config.php
+++ config/config.php
@@ -210,6 +210,7 @@ return array (
 		'key'                   => '',
 		'secret'                => '',
 	),
+	'google-verification'		=> '',
 	'calendar' 					=>  '',
 	'lastfm' => array (
 		'apikey'				=> '',
