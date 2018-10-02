bcf4d91e425034368178baa17771e3640397a36d
Cene <cene@cene.co.za>
2018-10-02 14:58:48 +0200

    rebuild after stable merge.

--- lib/boot.app.php
+++ lib/boot.app.php
@@ -9,7 +9,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Processor\PsrLogMessageProcessor;
 
-define('_APP_VERSION', '2.3.25'); // auto-generated: 1538484841367
+define('_APP_VERSION', '2.3.25'); // auto-generated: 1538485092119
 define('_BASEDIR', realpath(__DIR__ . '/../'));
 
 $loader = require _BASEDIR . '/vendor/autoload.php';
