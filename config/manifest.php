eb86afb1c5d9ced5afdc40d5accb52c08512bfbb
Cene <cene@cene.co.za>
2018-12-11 09:50:34 +0200

    updated to latest chat

--- config/manifest.php
+++ config/manifest.php
@@ -1,10 +1,10 @@
 <?php
-// auto-generated: 1544479815921
+// auto-generated: 1544514574954
 return [
 	"common.vendor.js" => "common.vendor.9445ea86023969226bce.js",
 	"runtime.js" => "runtime.ffaf452731b773b5b33e.js",
-	"chat.vendor.css" => "chat.vendor.809e84130022ccaa097d.css",
-	"chat.vendor.js" => "chat.vendor.650f541de6d4e4557aa9.js",
+	"chat.vendor.css" => "chat.vendor.d0e3400e3b32b143a246.css",
+	"chat.vendor.js" => "chat.vendor.be87749d3dc1b07e86d0.js",
 	"admin.js" => "admin.b36e6e6a18130e4ab646.js",
 	"chat.js" => "chat.232c943284c176a0f150.js",
 	"profile.js" => "profile.f8645f2f657bf4920d07.js",
