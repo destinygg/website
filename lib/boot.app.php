abba8956d41f16bc48dd8e8c3658d4c032aecd3e
Cene <cene@cene.co.za>
2019-01-31 00:08:55 +0200

    version bump for site migration / caching issues.

--- lib/boot.app.php
+++ lib/boot.app.php
@@ -9,7 +9,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Processor\PsrLogMessageProcessor;
 
-define('_APP_VERSION', '2.7.4'); // auto-generated: 1544545737610
+define('_APP_VERSION', '2.7.6'); // auto-generated: 1548886070288
 define('_BASEDIR', realpath(__DIR__ . '/../'));
 
 $loader = require _BASEDIR . '/vendor/autoload.php';
--- lib/boot.test.php
+++ lib/boot.test.php
@@ -8,7 +8,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Processor\PsrLogMessageProcessor;
 
-define('_APP_VERSION', '2.7.4'); // auto-generated: 1544545737627
+define('_APP_VERSION', '2.7.6'); // auto-generated: 1548886070300
 define('_BASEDIR', realpath(__DIR__ . '/../'));
 
 $loader = require _BASEDIR . '/vendor/autoload.php';
