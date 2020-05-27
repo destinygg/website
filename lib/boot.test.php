7697dc8691cd875da7ca74e5779ced22cafd1a82
Farhad Jay <effejaye@gmail.com>
2020-05-27 00:06:25 -0700

    Bump version number

--- lib/boot.test.php
+++ lib/boot.test.php
@@ -8,7 +8,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Processor\PsrLogMessageProcessor;
 
-define('_APP_VERSION', '2.14.1'); // auto-generated: 1589506413544
+define('_APP_VERSION', '2.14.2'); // auto-generated: 1590563171212
 define('_BASEDIR', realpath(__DIR__ . '/../'));
 
 $loader = require _BASEDIR . '/vendor/autoload.php';
