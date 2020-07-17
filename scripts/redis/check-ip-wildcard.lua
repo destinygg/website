b4a97698fcc88ef8d6f6a6b99ec68e1c939f9f62
Farhad Jay <effejaye@gmail.com>
2020-07-16 23:47:27 -0700

    No longer skip elements when looping through IPs

--- scripts/redis/check-ip-wildcard.lua
+++ scripts/redis/check-ip-wildcard.lua
@@ -6,9 +6,9 @@ local ip = ARGV[1]
 local foundusers = {}
 local keys = redis.call("KEYS", "CHAT:userips-*")
 for _, key in ipairs(keys) do
-    local rawData = redis.call('ZRANGE', key, 0, -1);
-    for idx = 1, #rawData, 2 do
-        if string.match(rawData[idx], ip) then
+    local userIps = redis.call('ZRANGE', key, 0, -1);
+    for _, userIp in ipairs(userIps) do
+        if string.match(userIp, ip) then
            table.insert(foundusers, key)
         end
     end
