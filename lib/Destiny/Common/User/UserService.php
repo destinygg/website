006c95e6ba6e4a59b5c9fa62e9e6e5d2d09d0468
Farhad Jay <effejaye@gmail.com>
2020-08-24 21:35:12 -0700

    Escape all underscores when searching for users
    Underscores behave as single-character wildcards in MySQL `LIKE`
    queries. Considering how common they are in usernames and how rarely
    (read: never) they're used as actual wildcards when searching for users
    on `/admin/users`, escaping them automatically is a sensible approach.

--- lib/Destiny/Common/User/UserService.php
+++ lib/Destiny/Common/User/UserService.php
@@ -513,9 +513,12 @@ class UserService extends Service {
                 $stmt->bindValue('roleId', $params['role'], PDO::PARAM_INT);
             }
             if (!empty($params['search'])) {
-                $stmt->bindValue('exactMatch', $params['search'], PDO::PARAM_STR);
-                $stmt->bindValue('beginsMatch', $params['search'] . '%', PDO::PARAM_STR);
-                $stmt->bindValue('containsMatch', '%' . $params['search'] . '%', PDO::PARAM_STR);
+                // Treat all underscores as literal underscores.
+                $modifiedSearch = str_replace('_', '\_', $params['search']);
+
+                $stmt->bindValue('exactMatch', $modifiedSearch, PDO::PARAM_STR);
+                $stmt->bindValue('beginsMatch', $modifiedSearch . '%', PDO::PARAM_STR);
+                $stmt->bindValue('containsMatch', '%' . $modifiedSearch . '%', PDO::PARAM_STR);
             }
             if (!empty($params['status'])) {
                 $stmt->bindValue('userStatus', $params['status'], PDO::PARAM_STR);
