3fb67517fc377f837d3258d6e285b552d9d8a0a5
Farhad Jay <effejaye@gmail.com>
2020-11-03 19:05:36 -0800

    Version bump

--- lib/boot.app.php
+++ lib/boot.app.php
@@ -11,7 +11,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Processor\PsrLogMessageProcessor;
 
-define('_APP_VERSION', '2.23.0'); // auto-generated: 1604255434303
+define('_APP_VERSION', '2.23.1'); // auto-generated: 1604459130456
 define('_BASEDIR', realpath(__DIR__ . '/../'));
 
 $loader = require _BASEDIR . '/vendor/autoload.php';
