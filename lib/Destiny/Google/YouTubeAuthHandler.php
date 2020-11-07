19c69daa4599a8fe8fef17e556366aacd0dceefc
Farhad Jay <effejaye@gmail.com>
2020-11-07 10:19:52 -0800

    Get YouTube channel info when authenticated

--- lib/Destiny/Google/YouTubeAuthHandler.php
+++ lib/Destiny/Google/YouTubeAuthHandler.php
@@ -2,9 +2,15 @@
 namespace Destiny\Google;
 
 use Destiny\Common\Authentication\AuthProvider;
+use Destiny\Common\Authentication\OAuthResponse;
+use Destiny\Common\Config;
+use Destiny\Common\Exception;
 use Destiny\Common\Session\Session;
+use Destiny\Common\Utils\FilterParams;
+use Destiny\Common\Utils\Http;
 
 class YouTubeAuthHandler extends GoogleAuthHandler {
+    private $apiBase = 'https://www.googleapis.com/youtube/v3';
     public $authProvider = AuthProvider::YOUTUBE;
 
     function getAuthorizationUrl(
@@ -17,5 +23,52 @@ class YouTubeAuthHandler extends GoogleAuthHandler {
         $claims = ''
     ): string {
         return parent::getAuthorizationUrl($scope, $claims);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getUserChannelIds(string $accessToken): array {
+        $client = $this->getHttpClient();
+        $response = $client->get("$this->apiBase/channels", [
+            'query' => [
+                'part' => 'snippet,id',
+                'mine' => true
+            ],
+            'headers' => [
+                'User-Agent' => Config::userAgent(),
+                'Authorization' => "Bearer $accessToken"
+            ]
+        ]);
+
+        if ($response->getStatusCode() == Http::STATUS_OK) {
+            return json_decode($response->getBody(), true);
+        }
+
+        throw new Exception('Error getting YouTube channels.');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function mapTokenResponse(array $token): OAuthResponse {
+        $data = $this->getUserChannelIds($token['access_token']);
+        FilterParams::required($data, 'items');
+
+        if (count($data['items']) < 1) {
+            throw new Exception('No YouTube channels exist.');
+        }
+
+        $firstChannel = $data['items'][0];
+        return new OAuthResponse([
+            'accessToken' => $token['access_token'],
+            'refreshToken' => $token['refresh_token'],
+            'authProvider' => $this->authProvider,
+            'username' => $firstChannel['snippet']['title'],
+            'authId' => $firstChannel['id'],
+            'authDetail' => $firstChannel['snippet']['title'],
+            'authEmail' => '',
+            'verified' => true,
+        ]);
     }
 }
