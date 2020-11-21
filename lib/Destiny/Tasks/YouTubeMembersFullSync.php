e41a9a0a95d93d72e83f34a035e3a89b33a97286
Farhad Jay <effejaye@gmail.com>
2020-11-21 14:54:37 -0800

    Fetch & sync all YT memberships every 24 hours
    This allows us to purge expired memberships from the database once per
    day. We also update membership levels to handle `channelId` changes (in
    `config.php`) and membership display name changes.

---
+++ lib/Destiny/Tasks/YouTubeMembersFullSync.php
@@ -0,0 +1,39 @@
+<?php
+namespace Destiny\Tasks;
+
+use Destiny\Common\Annotation\Schedule;
+use Destiny\Common\Authentication\AuthenticationService;
+use Destiny\Common\Cron\TaskInterface;
+use Destiny\YouTube\YouTubeAdminApiService;
+use Destiny\YouTube\YouTubeMembershipService;
+
+/**
+ * @Schedule(frequency=24,period="hour")
+ */
+class YouTubeMembersFullSync implements TaskInterface {
+    public function execute() {
+        $this->syncMembershipLevels();
+        $this->syncMemberships();
+    }
+
+    private function syncMembershipLevels() {
+        $youTubeAdminApiService = YouTubeAdminApiService::instance();
+        $membershipLevels = $youTubeAdminApiService->getMembershipLevels();
+
+        $youTubeMembershipService = YouTubeMembershipService::instance();
+        $youTubeMembershipService->addMembershipLevels($membershipLevels);
+    }
+
+    private function syncMemberships() {
+        $youTubeAdminApiService = YouTubeAdminApiService::instance();
+        $allMemberships = $youTubeAdminApiService->getAllMemberships();
+
+        $youTubeMembershipService = YouTubeMembershipService::instance();
+        $channelIdsToUpdate = $youTubeMembershipService->syncMemberships($allMemberships);
+        $userIdsToUpdate = $youTubeMembershipService->getUserIdsForChannelIds($channelIdsToUpdate);
+
+        foreach ($userIdsToUpdate as $userIdToUpdate) {
+            AuthenticationService::instance()->flagUserForUpdate($userIdsToUpdate);
+        }
+    }
+}
