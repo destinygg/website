3c0e1d331cfc3c9337b53cf630c611aa2b71f9c5
Farhad Jay <effejaye@gmail.com>
2020-11-21 15:24:40 -0800

    Create task to fetch new members every 5 minutes

---
+++ lib/Destiny/Tasks/YouTubeGetNewMembers.php
@@ -0,0 +1,25 @@
+<?php
+namespace Destiny\Tasks;
+
+use Destiny\Common\Annotation\Schedule;
+use Destiny\Common\Authentication\AuthenticationService;
+use Destiny\Common\Cron\TaskInterface;
+use Destiny\YouTube\YouTubeAdminApiService;
+use Destiny\YouTube\YouTubeMembershipService;
+
+/**
+ * @Schedule(frequency=5,period="minute")
+ */
+class YouTubeGetNewMembers implements TaskInterface {
+    public function execute() {
+        $youTubeAdminApiService = YouTubeAdminApiService::instance();
+        $newMemberships = $youTubeAdminApiService->getNewMemberships();
+
+        $youTubeMembershipService = YouTubeMembershipService::instance();
+        $channelIdsToUpdate = $youTubeMembershipService->addMemberships($newMemberships);
+        $userIdsToUpdate = $youTubeMembershipService->getUserIdsForChannelIds($channelIdsToUpdate);
+        foreach ($userIdsToUpdate as $userIdToUpdate) {
+            AuthenticationService::instance()->flagUserForUpdate($userIdsToUpdate);
+        }
+    }
+}
