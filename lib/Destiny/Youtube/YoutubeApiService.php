c595ed19c1e3f6f05b0c1d3b80ec8068cfe06d14
Farhad Jay <effejaye@gmail.com>
2020-11-22 15:14:07 -0800

    Add API func for fetching a user's YT channel IDs

--- lib/Destiny/Youtube/YoutubeApiService.php
+++ lib/Destiny/Youtube/YoutubeApiService.php
@@ -1,11 +1,46 @@
 <?php
 namespace Destiny\Youtube;
 
+use Destiny\Common\Authentication\AuthProvider;
+use Destiny\Common\Config;
+use Destiny\Common\HttpClient;
 use Destiny\Common\Service;
+use Destiny\Common\User\UserAuthService;
+use Destiny\Common\Utils\Http;
 
 /**
  * @method static YoutubeApiService instance()
  */
 class YouTubeApiService extends Service {
+    private $apiBase = 'https://www.googleapis.com/youtube/v3';
+    private $provider = AuthProvider::YOUTUBE;
 
+    /**
+     * @throws Exception
+     */
+    public function getChannelIdsForUserId(int $userId): array {
+        $oauthDetails = UserAuthService::instance()->getByUserIdAndProvider($userId, $this->provider);
+        if (empty($oauthDetails)) {
+            throw Exception("Error getting YT channel IDs because no OAuth details exist for user `$userId`.");
+        }
+
+        $client = HttpClient::instance();
+        $response = $client->get("$this->apiBase/channels", [
+            'headers' => [
+                'User-Agent' => Config::userAgent(),
+                'Authorization' => "Bearer {$oauthDetails['accessToken']}"
+            ],
+            'query' => [
+                'part' => 'id',
+                'mine' => 'true'
+            ]
+        ]);
+
+        if ($response->getStatusCode() !== Http::STATUS_OK) {
+            throw Exception("Got a non-200 response when fetching YouTube channels for user `$userId`: `$response->getBody()`.");
+        }
+
+        $json = json_decode($response->getBody(), true);
+        return $json['items'];
+    }
 }
