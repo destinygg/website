66eefcae05dd55adcd7f621c774d7c4ef596331d
Farhad Jay <effejaye@gmail.com>
2021-05-02 19:23:14 -0700

    Cache info on latest YT livestream

--- lib/Destiny/Tasks/YouTubeTasks.php
+++ lib/Destiny/Tasks/YouTubeTasks.php
@@ -7,6 +7,7 @@ use Destiny\Common\Config;
 use Destiny\Common\Cron\TaskInterface;
 use Destiny\Common\Log;
 use Destiny\Common\Images\ImageDownloadUtil;
+use Destiny\Common\Utils\Date;
 use Destiny\YouTube\YouTubeAdminApiService;
 
 /**
@@ -15,6 +16,7 @@ use Destiny\YouTube\YouTubeAdminApiService;
 class YouTubeTasks implements TaskInterface {
     const RECENT_YOUTUBE_UPLOADS_CACHE_KEY = 'youtubeplaylist';
     const RECENT_YOUTUBE_LIVESTREAM_VODS_CACHE_KEY = 'youtubevods';
+    const YOUTUBE_LIVESTREAM_STATUS_CACHE_KEY = 'ytstreamstatus';
     const MAX_RECENT_VIDEO_UPLOADS = 4;
     const MAX_RECENT_LIVESTREAM_VODS = 4;
 
@@ -23,6 +25,7 @@ class YouTubeTasks implements TaskInterface {
             $videos = YouTubeAdminApiService::instance()->getRecentYouTubeUploads();
             $this->updateRecentVideoUploads($videos);
             $this->updateRecentLivestreamVODs($videos);
+            $this->updateLivestreamStatus($videos);
         } catch (Exception $e) {
             Log::error("Fetching recent YouTube uploads failed. {$e->getMessage()}");
         }
@@ -74,6 +77,42 @@ class YouTubeTasks implements TaskInterface {
 
         $cache = Application::getNsCache();
         $cache->save(self::RECENT_YOUTUBE_LIVESTREAM_VODS_CACHE_KEY, $normalizedBroadcasts);
+    }
+
+    public function updateLivestreamStatus(array $videos) {
+        // Get the latest broadcast.
+        $currentBroadcast = null;
+        foreach ($videos as $video) {
+            if (!empty($video['liveStreamingDetails'])) {
+                $currentBroadcast = $video;
+                break; 
+            }
+        }
+
+        if (empty($currentBroadcast)) {
+            Log::warning('No YouTube livestreams found.');
+            return;
+        }
+
+        $actualStartTime = $currentBroadcast['snippet']['actualStartTime'] ?? null;
+        $startTime = !empty($actualStartTime) ? Date::getDateTime($actualStartTime) : null;
+
+        $actualEndTime = $currentBroadcast['snippet']['actualEndTime'] ?? null;
+        $endTime = !empty($actualEndTime) ? Date::getDateTime($actualEndTime) : null;
+
+        $livestreamStatus = [
+            'live' => !empty($startTime) && empty($endTime),
+            'game' => null,
+            'status_text' => $currentBroadcast['snippet']['title'],
+            'preview' => $currentBroadcast['snippet']['thumbnails']['medium']['url'],
+            'started_at' => !empty($startTime) ? $startTime->format(Date::FORMAT) : null,
+            'ended_at' => !empty($endTime) ? $endTime->format(Date::FORMAT) : null,
+            'duration' => !empty($startTime) ? time() - $startTime->getTimestamp() : null,
+            'viewers' => $currentBroadcast['liveStreamingDetails']['concurrentViewers'],
+        ];
+
+        $cache = Application::getNsCache();
+        $cache->save(self::YOUTUBE_LIVESTREAM_STATUS_CACHE_KEY, $livestreamStatus);
     }
 
     private function urlForVideo(array $video): string {
