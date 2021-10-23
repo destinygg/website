ae254e53d548330507024dede51bea19a7269a75
Farhad Jay <effejaye@gmail.com>
2021-10-23 08:56:08 -0700

    Fix bug with required argument in logging funcs

--- lib/Destiny/Common/Log.php
+++ lib/Destiny/Common/Log.php
@@ -10,27 +10,27 @@ class Log {
      */
     public static $log;
 
-    public static function info(string $message, ?array $ctx){
+    public static function info(string $message, ?array $ctx = []) {
         self::$log->info($message, $ctx ?? []);
     }
 
-    public static function warn(string $message, ?array $ctx){
+    public static function warn(string $message, ?array $ctx = []) {
         self::$log->warning($message, $ctx ?? []);
     }
 
-    public static function notice(string $message, ?array $ctx){
+    public static function notice(string $message, ?array $ctx = []) {
         self::$log->notice($message, $ctx ?? []);
     }
 
-    public static function debug(string $message, ?array $ctx){
+    public static function debug(string $message, ?array $ctx = []) {
         self::$log->debug($message, $ctx ?? []);
     }
 
-    public static function error(string $message, ?array $ctx){
+    public static function error(string $message, ?array $ctx = []) {
         self::$log->error($message, $ctx ?? []);
     }
 
-    public static function critical(string $message, ?array $ctx){
+    public static function critical(string $message, ?array $ctx = []) {
         self::$log->critical($message, $ctx ?? []);
     }
 
